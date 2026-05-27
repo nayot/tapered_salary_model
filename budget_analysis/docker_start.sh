e3561fb9118b54753c6a439b0fc648e2bba30c2f
Nayot <nayot@eng.buu.ac.th>
2026-05-28 00:02:11 +0700

    new file:   docker_start.sh

---
+++ budget_analysis/docker_start.sh
@@ -0,0 +1,4 @@
+SALARY_DATA_DIR=/home/nayot/dockers/production/tapered_salary_model/budget_analysis/data \ 
+STREAMLIT_SECRETS_FILE=/home/nayot/dockers/production/tapered_salary_model/budget_analysis/.streamlit/secrets.toml \
+STREAMLIT_PORT=8501 \
+docker compose up -d 
